0822dd901c0692de3bd4a93114cf36c464df9fa5
Joseph Dehoney <josephdehoney@gmail.com>
2021-03-26 21:04:49 -0700

    Added functioning minimizeable sidebar for navbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,37 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    //closeNav();
+})
+
+var nav = false;
+
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("navbar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    nav = true;
 }
   
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("navbar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    nav = false;
 }
+
+function toggleNav() {
+    nav ? closeNav() : openNav();
+}
+
+// function navChanger() {
+//     var nav = document.getElementById("navbar");
+//     if (nav.style.display === "none"){
+//         nav.style.display = "block";
+//     } else {
+//         nav.style.display = "none";
+//     }
+// }
+
+// const elementClicked = document.getElementById("changeButton");
+// const elementYouWantToShow = document.getElementById("navbar");
+
+// elementClicked.addEventListener("click", ()=>{
+//   elementYouWantToShow.classList.toggle("hide");
+// });
